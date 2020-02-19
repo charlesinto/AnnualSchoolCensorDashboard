b77bd613dca5ea4952efcba0eacf04c0d127aeb8
charlesinto <charles.onuorah@yahoo.com>
2020-02-19 11:20:16 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { JQUERY_TOKEN } from './services/jquery.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'asc-web';
+  constructor(@Inject(JQUERY_TOKEN) private $: any) { }
+  ngOnInit() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { JQUERY_TOKEN } from './services/jquery.service';
+
+
+ const jquery = window['$'];
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +18,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: JQUERY_TOKEN, useValue: jquery}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
