f9583284a36e2a94595063ad02ff06c6de1330a7
charlesinto <charles.onuorah@yahoo.com>
2020-02-21 16:43:01 +0100

    Make Navbar header position fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ManageroleComponent } from "./managerole/managerole.component";
 
 const routes: Routes = [
   {path: 'login', pathMatch: 'full', component: LoginComponent},
+  {path: 'manage-roles', pathMatch: 'full', component: ManageroleComponent},
   {path: '', pathMatch: 'full', component: HomeComponent }
 ];
 
---
+++ src/app/managerole/managerole.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-managerole',
+  templateUrl: './managerole.component.html',
+  styleUrls: ['./managerole.component.scss']
+})
+export class ManageroleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
